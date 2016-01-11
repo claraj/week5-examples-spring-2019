8f66b89321b722c106e210f0b6d625c3d5aa9b98
clara <clara@clara.com>
2016-01-10 18:26:08 -0600

    Week 4 Examples

--- first-class-definition.js
+++ first-class-definition.js
@@ -1,4 +1,5 @@
 
+//Acts as our class definition - define a constructor
 var Squirrel = function(n) {
   this.name = n;
   this.nuts = 0;
@@ -35,9 +36,21 @@ squeaky.faveFood();
 squeaky.sayHello();
 //Can you call addNutsToStore for this Squirrel?
 
+
+
 //Adding properties to one Squirrel only
 squeaky.tree = "Oak Tree";
 
 console.log("Squeaky's tree is : "+ squeaky.tree);   // "Oak Tree"
 //Fluffy doesn't have a tree
 console.log("Fluffy's tree is : "+ fluffy.tree);   // "undefined"
+
+//Add a jump method only for fluffy
+fluffy.jump = function() {
+  console.log(this.name + " is jumping!");
+};
+
+fluffy.jump();   //This works
+
+// squeaky.jump();  //Calling this results in an error - squeaky doesn't have a jump method
+
