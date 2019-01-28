b495a7bacdf0d272cd15c986e982f394fc5421f7
clara <me@me.com>
2019-01-28 14:03:54 -0600

    leaflet example

---
+++ leaflet_maps/maps.js
@@ -0,0 +1,33 @@
+
+let mctcCoordinates = [44.9724, -93.2844]
+let stPaulCoordinates = [44.94839, -93.1099]
+let metroAreaCenterCoordinates = [44.96, -93.2]
+
+// Create the map 
+let map = L.map('mctc-map').setView(metroAreaCenterCoordinates, 9)
+
+// Add the tile layer - roads, streets etc. Without this, nothing to see 
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiY2xhcmFsIiwiYSI6ImNqcmdwenViYTAwcHQ0Ym5yYmZ1Z3E2bjgifQ.QQfUvVaqPsWb_jJbP2gvHg'
+}).addTo(map)
+
+// Add some markers 
+var mctcMarker = L.marker(mctcCoordinates)
+    .bindPopup("Minnepolis College<br><a href='https://minneapolis.edu'>Website</a>")
+    .addTo(map)
+
+var stpMarker = L.marker(stPaulCoordinates)
+    .bindPopup("Saint Paul College<br><a href='https://saintpaul.edu'>Website</a>")
+    .addTo(map)
+
+// Add a circle 
+var metroAreaCircle = L.circle(metroAreaCenterCoordinates, {
+    color: 'green',  
+    radius: 30000,
+    fillOpacity: 0.2
+})
+.bindPopup("Twin Cities Metro Area")
+.addTo(map)
