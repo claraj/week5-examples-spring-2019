2ba10d5f12c056c2d739e0a3e40c1d5b8aba77d1
clara <me@me.com>
2019-01-28 14:19:40 -0600

    chartjs example

---
+++ chart_js/hello_chart.js
@@ -0,0 +1,23 @@
+
+var canvas = document.getElementById('soda-chart')
+console.log(canvas)
+var ctx = canvas.getContext('2d')
+
+chart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Coke", "Pepsi", "Either", "Neither"],
+        datasets: [{
+            label: 'Number of votes',
+            data: [18, 14, 7, 10]
+        }]
+    }, options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+})
